93fb33429bd4d8f4a2d831e50d1a9c3e42bd0103
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-01-30 22:55:32 -0800

    fixed the issue with not getting folder into github

---
+++ 4-React-Native/nucampsite/components/AboutComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { ScrollView } from '';
+
+class About extends Component {
+    render() {
+        return (
+            <ScrollView>
+
+            </ScrollView>
+        )
+    }
+}
+
+export default About;
---
+++ 4-React-Native/nucampsite/components/ContactComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { ScrollView } from '';
+
+class Contact extends Component {
+    render() {
+        return (
+            <ScrollView>
+
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
