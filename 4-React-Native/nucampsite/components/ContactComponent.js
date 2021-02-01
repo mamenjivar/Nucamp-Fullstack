92e2c7e6d8c804cc58fa352cfa093080a7fd5ec8
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-01-31 16:08:38 -0800

    made new changes and finished task 2 of week 01 assignment

--- 4-React-Native/nucampsite/components/ContactComponent.js
+++ 4-React-Native/nucampsite/components/ContactComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { Text } from 'react-native';
 
 class Contact extends Component {
 
@@ -10,7 +12,18 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{ margin: 20 }}
+                >
+                    <Text>1 NuCamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10 }}>U.S.A.</Text>
+                
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
 
+                </Card>
             </ScrollView>
         );
     }
